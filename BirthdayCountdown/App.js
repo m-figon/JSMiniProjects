01c5af36d1726ba63f488cabef86b3d8e81fe7aa
mateusz <dalvan234@gmail.com>
2020-04-10 20:41:35 +0200

    rotation animation after minute change added

--- BirthdayCountdown/App.js
+++ BirthdayCountdown/App.js
@@ -5,6 +5,7 @@ const secondsNumber = document.querySelector("#seconds");
 const app = document.querySelector(".app");
 const title = document.querySelector(".title");
 const timer = document.querySelector(".timer");
+const units = document.querySelectorAll(".unit");
 
 
 const MILLI_SECOND = 1;
@@ -14,7 +15,10 @@ const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;
 
 var currentDate = new Date();
-const birthdayDate = new Date("April 10, 2020 18:17:00");
+const birthdayDate = new Date("April 10, 2020 20:43:40");
+
+var days,hours,minutes,seconds;
+var tmpDays, tmpHours, tmpMinutes, tmpSeconds;
 
 const displaySurprise = () => {    
     app.removeChild(timer);
@@ -27,11 +31,25 @@ const checkAndPrintTime = () => {
     }
     var currentDate = new Date();
     if (birthdayDate.getTime() - currentDate.getTime() >= 0) {
-        var days = Math.floor((birthdayDate.getTime() - currentDate.getTime()) / (DAY));
-        var hours = Math.floor((birthdayDate.getTime() - currentDate.getTime() - (days * DAY)) / (HOUR));
-        var minutes = Math.floor((birthdayDate.getTime() - currentDate.getTime() - (days * DAY) - (hours * HOUR)) / (MINUTE));
-        var seconds = Math.floor(((birthdayDate.getTime() - currentDate.getTime()) - (days * DAY) - (hours * HOUR) - (minutes * MINUTE)) / (SECOND));
+        days = Math.floor((birthdayDate.getTime() - currentDate.getTime()) / (DAY));
+        hours = Math.floor((birthdayDate.getTime() - currentDate.getTime() - (days * DAY)) / (HOUR));
+        minutes = Math.floor((birthdayDate.getTime() - currentDate.getTime() - (days * DAY) - (hours * HOUR)) / (MINUTE));
+        seconds = Math.floor(((birthdayDate.getTime() - currentDate.getTime()) - (days * DAY) - (hours * HOUR) - (minutes * MINUTE)) / (SECOND));
         
+        if(tmpMinutes!==minutes){
+            console.log("minute change!");
+            minutesNumber.style.animation = "mymove 0.5s steps(50)";
+            for(const element of units){
+                timer.removeChild(element);
+            }
+            for(const element of units){
+                timer.appendChild(element);
+            }
+        }
+        tmpDays = days;
+        tmpHours = hours;
+        tmpMinutes = minutes;
+        tmpSeconds = seconds;
         displayNumber(days, daysNumber);
         displayNumber(hours, hoursNumber);
         displayNumber(minutes, minutesNumber);
