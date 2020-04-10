9f7ff8b585b39a9688770adbfb48e6d945fbbe63
mateusz <dalvan234@gmail.com>
2020-04-10 17:40:43 +0200

    displaying new text when countdown is over

--- BirthdayCountdown/App.js
+++ BirthdayCountdown/App.js
@@ -2,35 +2,50 @@ const daysNumber = document.querySelector("#days");
 const hoursNumber = document.querySelector("#hours");
 const minutesNumber = document.querySelector("#minutes");
 const secondsNumber = document.querySelector("#seconds");
+const app = document.querySelector(".app");
+const title = document.querySelector(".title");
+const timer = document.querySelector(".timer");
+
 
 const MILLI_SECOND = 1;
-const SECOND = 1000*MILLI_SECOND;
-const MINUTE = SECOND*60;
-const HOUR = MINUTE*60;
-const DAY = HOUR*24;
+const SECOND = 1000 * MILLI_SECOND;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
 
-var birthdayDate = new Date("April 10, 2020 00:00:00");
+var currentDate = new Date();
+const birthdayDate = new Date("April 10, 2020 18:17:00");
 
-const checkAndPrintTime = () =>{
-    function displayNumber(value,type){
-        (value<10)?type.textContent="0"+value:type.textContent=value;
+const displaySurprise = () => {    
+    app.removeChild(timer);
+    console.log(title.children[0].innerText="Happy Birthday!");
+}
+
+const checkAndPrintTime = () => {
+    function displayNumber(value, type) {
+        (value < 10 && value >= 0) ? type.textContent = "0" + value : type.textContent = value;
     }
     var currentDate = new Date();
-    var days = Math.floor((birthdayDate.getTime() - currentDate.getTime())/(DAY));
-    var hours = Math.floor((birthdayDate.getTime() - currentDate.getTime()-(days*DAY))/(HOUR));
-    var minutes = Math.floor((birthdayDate.getTime() - currentDate.getTime()-(days*DAY)-(hours*HOUR))/(MINUTE));
-    var seconds = Math.floor(((birthdayDate.getTime() - currentDate.getTime())-(days*DAY)-(hours*HOUR)-(minutes*MINUTE))/(SECOND));
-    
-    displayNumber(days,daysNumber);
-    displayNumber(hours,hoursNumber);
-    displayNumber(minutes,minutesNumber);
-    displayNumber(seconds,secondsNumber);
-
+    if (birthdayDate.getTime() - currentDate.getTime() >= 0) {
+        var days = Math.floor((birthdayDate.getTime() - currentDate.getTime()) / (DAY));
+        var hours = Math.floor((birthdayDate.getTime() - currentDate.getTime() - (days * DAY)) / (HOUR));
+        var minutes = Math.floor((birthdayDate.getTime() - currentDate.getTime() - (days * DAY) - (hours * HOUR)) / (MINUTE));
+        var seconds = Math.floor(((birthdayDate.getTime() - currentDate.getTime()) - (days * DAY) - (hours * HOUR) - (minutes * MINUTE)) / (SECOND));
+        
+        displayNumber(days, daysNumber);
+        displayNumber(hours, hoursNumber);
+        displayNumber(minutes, minutesNumber);
+        displayNumber(seconds, secondsNumber);
+    }else{
+        displaySurprise();
+        setInterval("javascript function", milliseconds);
+    }
 }
 
 checkAndPrintTime();
+setInterval(checkAndPrintTime, 1000)
 
-setInterval(checkAndPrintTime,1000)
+
 
 
 
