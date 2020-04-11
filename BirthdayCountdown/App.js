8d3bc5e8f4f32b35ffa066ebd8f6387f49dcded3
mateusz <dalvan234@gmail.com>
2020-04-11 11:30:20 +0200

    enter date value functionality added and changing page appearance at the start

--- BirthdayCountdown/App.js
+++ BirthdayCountdown/App.js
@@ -9,7 +9,8 @@ const unitDay = document.querySelector("#day");
 const unitHour = document.querySelector("#hour");
 const unitMinute = document.querySelector("#minute");
 const unitSecond = document.querySelector("#second");
-
+const button = document.querySelector("button");
+const input = document.querySelector("input");
 
 const MILLI_SECOND = 1;
 const SECOND = 1000 * MILLI_SECOND;
@@ -18,17 +19,19 @@ const HOUR = MINUTE * 60;
 const DAY = HOUR * 24;
 
 var currentDate = new Date();
-const birthdayDate = new Date("April 10, 2020 23:43:40");
+var birthdayDate = new Date();
+var days, hours, minutes, seconds;
+var tmpDays, tmpHours, tmpMinutes, tmpSeconds, started;
 
-var days,hours,minutes,seconds;
-var tmpDays, tmpHours, tmpMinutes, tmpSeconds;
-
-const displaySurprise = () => {    
+const displaySurprise = () => {
     app.removeChild(timer);
-    console.log(title.children[0].innerText="Happy Birthday!");
+    title.children[0].innerText = "Happy Birthday!";
+    var image = document.createElement("img");
+    image.src = "http://www.pngall.com/wp-content/uploads/2016/07/Birthday-Present-PNG-Pic.png"
+    title.appendChild(image);
 }
-function makeAnimation(condition1,condition2,value1,value2){
-    if(condition1!==condition2){
+function makeAnimation(condition1, condition2, value1, value2) {
+    if (condition1 !== condition2) {
         value1.style.animation = "mymove 0.5s steps(50)";
         value2.removeChild(value1);
         value2.appendChild(value1);
@@ -40,31 +43,48 @@ const checkAndPrintTime = () => {
     }
     var currentDate = new Date();
     if (birthdayDate.getTime() - currentDate.getTime() >= 0) {
-        days = Math.floor((birthdayDate.getTime() - currentDate.getTime()) / (DAY));
-        hours = Math.floor((birthdayDate.getTime() - currentDate.getTime() - (days * DAY)) / (HOUR));
-        minutes = Math.floor((birthdayDate.getTime() - currentDate.getTime() - (days * DAY) - (hours * HOUR)) / (MINUTE));
-        seconds = Math.floor(((birthdayDate.getTime() - currentDate.getTime()) - (days * DAY) - (hours * HOUR) - (minutes * MINUTE)) / (SECOND));
+        if (started) {
+            days = Math.floor((birthdayDate.getTime() - currentDate.getTime()) / (DAY));
+            hours = Math.floor((birthdayDate.getTime() - currentDate.getTime() - (days * DAY)) / (HOUR));
+            minutes = Math.floor((birthdayDate.getTime() - currentDate.getTime() - (days * DAY) - (hours * HOUR)) / (MINUTE));
+            seconds = Math.floor(((birthdayDate.getTime() - currentDate.getTime()) - (days * DAY) - (hours * HOUR) - (minutes * MINUTE)) / (SECOND));
 
-        makeAnimation(tmpMinutes,minutes,unitMinute,minutesNumber);
-        makeAnimation(tmpHours,hours,unitHour,hoursNumber);
-        makeAnimation(tmpDays,days,unitDay,daysNumber);
+            makeAnimation(tmpMinutes, minutes, unitMinute, minutesNumber);
+            makeAnimation(tmpHours, hours, unitHour, hoursNumber);
+            makeAnimation(tmpDays, days, unitDay, daysNumber);
 
-        tmpDays = days;
-        tmpHours = hours;
-        tmpMinutes = minutes;
-        tmpSeconds = seconds;
-        displayNumber(days, unitDay);
-        displayNumber(hours, unitHour);
-        displayNumber(minutes, unitMinute);
-        displayNumber(seconds, unitSecond);
-    }else{
-        displaySurprise();
-        setInterval("javascript function", milliseconds);
+            tmpDays = days;
+            tmpHours = hours;
+            tmpMinutes = minutes;
+            tmpSeconds = seconds;
+
+            displayNumber(days, unitDay);
+            displayNumber(hours, unitHour);
+            displayNumber(minutes, unitMinute);
+            displayNumber(seconds, unitSecond);
+        }
+
+    } else {
+        if (started) {
+            displaySurprise();
+            setInterval("javascript function", milliseconds);
+        }
+
     }
 }
+app.removeChild(timer);
+title.children[0].innerText = "Please enter date e.g.'April 29, 2020 00:00:00'";
+button.addEventListener("click", () => {
+    birthdayDate = new Date(input.value);
+    title.children[0].innerText = "Birthday Timer";
+    app.appendChild(timer);
+    input.style.display = "none";
+    button.style.display = "none";
+    started = true;
+    unitSecond.style.animation = "mymove 0.5s steps(50)";
+    checkAndPrintTime();
+})
 
-unitSecond.style.animation = "mymove 0.5s steps(50)";
-checkAndPrintTime();
 setInterval(checkAndPrintTime, 1000)
 
 
