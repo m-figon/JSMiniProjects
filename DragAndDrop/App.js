483ce57df8395f8b7185881e32d5938fa9919035
mateusz <dalvan234@gmail.com>
2020-04-12 22:22:50 +0200

    droping possible only when ball and field are correct

--- DragAndDrop/App.js
+++ DragAndDrop/App.js
@@ -2,8 +2,10 @@ const bottom = document.querySelector(".bottom");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const middle = document.querySelector(".middle");
+const bottomField = document.querySelector(".bottom > .field")
 const areasArray = [bottom, left, right, middle];
 var num, positionX, positionY;
+var correctDrops=0;
 
 var ball1 = document.createElement("img");
 var ball2 = document.createElement("img");
@@ -12,32 +14,30 @@ var ball4 = document.createElement("img");
 
 var balls = [ball1, ball2, ball3, ball4];
 
-
 function randomInt(min, max) {
     return min + Math.floor((max - min) * Math.random());
 }
 //random ball generating
-for (var ball of balls) {
-    num = randomInt(0, 3);
-    switch (num) {
-        case 0: ball.src = "soccer.png"; break;
-        case 1: ball.src = "basketball.png"; break;
-        case 2: ball.src = "volley.png"; break;
+const ballsInit = () =>{
+    for (var ball of balls) {
+        num = randomInt(0, 3);
+        switch (num) {
+            case 0: ball.src = "soccer.png"; break;
+            case 1: ball.src = "basketball.png"; break;
+            case 2: ball.src = "volley.png"; break;
+        }
+        num = randomInt(2, 9);
+        num = num + "0%";
+        ball.style.zIndex=3;
+        ball.style.left = num;
+        num = randomInt(7, 9.1);
+        num = num + "0%";
+        ball.style.top = num;
+        ball.draggable = "true";
+        bottom.firstElementChild.appendChild(ball); 
     }
-    num = randomInt(2, 9);
-    num = num + "0%";
-    ball.style.left = num;
-    num = randomInt(7, 9.1);
-    num = num + "0%";
-    ball.style.top = num;
-    bottom.firstElementChild.appendChild(ball); //adding ball
-    //
-    //ball draggable
-    ball.draggable = "true";
-    console.log(ball);
-
-
 }
+
 ball1.addEventListener("dragstart", event => {
     event.dataTransfer.setData("text/plain", "ball1");
     event.dataTransfer.effectAllowed = "move";
@@ -55,6 +55,18 @@ ball4.addEventListener("dragstart", event => {
     event.dataTransfer.effectAllowed = "move";
 })
 //
+const dropFunctionality = (dataSend,name,obj,obj2) =>{
+    if (dataSend === name) {
+        if((obj2.className==="left" && obj.src.match(/soccer/))|| (obj2.className==="middle" && obj.src.match(/basketball/)) || (obj2.className==="right" && obj.src.match(/volley/))){
+            obj.style.left = positionX + "px";
+            obj.style.top = positionY + "px";
+            correctDrops++;
+        }
+        if(correctDrops==4){
+            console.log('you won!');
+        }
+}
+}
 for (const area of areasArray) {
     area.firstElementChild.addEventListener("dragover", event => {
         event.preventDefault();
@@ -69,20 +81,10 @@ for (const area of areasArray) {
     area.addEventListener("drop", event => {
         area.firstElementChild.style.backgroundColor = "white";
         var data = event.dataTransfer.getData("text/plain");
-        console.log(data);
-        if (data === "ball1") {
-            ball1.style.left = positionX + "px";
-            ball1.style.top = positionY + "px";
-        } else if (data === "ball2") {
-            ball2.style.left = positionX + "px";
-            ball2.style.top = positionY + "px";
-        } else if (data === "ball3") {
-            ball3.style.left = positionX + "px";
-            ball3.style.top = positionY + "px";
-        } else if (data === "ball4") {
-            ball4.style.left = positionX + "px";
-            ball4.style.top = positionY + "px";
-        }
-
-    })
+        dropFunctionality(data,"ball1",ball1,area);
+        dropFunctionality(data,"ball2",ball2,area);
+        dropFunctionality(data,"ball3",ball3,area);
+        dropFunctionality(data,"ball4",ball4,area);
+    });
 }
+ballsInit();
