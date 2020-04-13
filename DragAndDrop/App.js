9ca898e7101fd199e43f18d969afb5efc2ec8ccc
mateusz <dalvan234@gmail.com>
2020-04-13 12:50:19 +0200

    not repeating positions of balls functionality added

--- DragAndDrop/App.js
+++ DragAndDrop/App.js
@@ -6,39 +6,56 @@ const app = document.querySelector(".app");
 
 const bottomField = document.querySelector(".bottom > .field")
 const areasArray = [bottom, left, right, middle];
-var num, positionX, positionY;
+var num, correctFlag, positionX, positionY;
 var correctDrops=0;
 
 var ball1 = document.createElement("img");
 var ball2 = document.createElement("img");
 var ball3 = document.createElement("img");
 var ball4 = document.createElement("img");
-
+var positionsX=[];
+var positionsY=[];
 var balls = [ball1, ball2, ball3, ball4];
 
 function randomInt(min, max) {
     return min + Math.floor((max - min) * Math.random());
 }
+const positionReset = () =>{
+    for(var i=0; i<4; i++){
+        positionsX[i]=0;
+        positionsY[i]=0;
+    }
+}
+const positionCheck = () =>{
+    for(var i=0; i<4; i++){
+        for(var j=0; j<4; j++){
+            if(positionsX[i]===positionsX[j] && positionsY[i]===positionsY[j] && i!==j){
+                return false;
+            }
+        }
+    }
+    return true;
+}
 //random ball generating
 const ballsInit = () =>{
-    for (var ball of balls) {
+    for (var i=0; i<4; i++) {
         num = randomInt(0, 3);
         switch (num) {
-            case 0: ball.src = "soccer.png"; break;
-            case 1: ball.src = "basketball.png"; break;
-            case 2: ball.src = "volley.png"; break;
+            case 0: balls[i].src = "soccer.png"; break;
+            case 1: balls[i].src = "basketball.png"; break;
+            case 2: balls[i].src = "volley.png"; break;
         }
-        num = randomInt(2, 9);
-        num = num + "0%";
-        ball.style.zIndex=3;
-        ball.style.left = num;
-        num = randomInt(7, 9.1);
-        num = num + "0%";
-        ball.style.top = num;
-        ball.draggable = "true";
-
-        bottom.firstElementChild.appendChild(ball); 
+        positionsX[i] = randomInt(2, 9);
+        positionsY[i] = randomInt(7, 9);
+        positionsX[i] = positionsX[i] + "0%";
+        positionsY[i] = positionsY[i] + "0%";
+        balls[i].style.zIndex=3;
+        balls[i].style.left = positionsX[i];
+        balls[i].style.top = positionsY[i];
+        balls[i].draggable = "true";
+        bottom.firstElementChild.appendChild(balls[i]); 
     }
+    correctFlag=positionCheck();
     ball1.addEventListener("dragstart", event => {
         event.dataTransfer.setData("text/plain", "ball1");
         event.dataTransfer.effectAllowed = "move";
@@ -76,7 +93,11 @@ const dropFunctionality = (dataSend,name,obj,obj2) =>{
             setTimeout(function() {
                 document.lastElementChild.lastElementChild.removeChild(signDiv);
                 document.lastElementChild.lastElementChild.appendChild(app);
-                ballsInit();
+                correctFlag=false;
+                while(!correctFlag){
+                    ballsInit();
+                }
+            
             }, 3000);
         }
 }
@@ -103,5 +124,7 @@ const areasInit = () =>{
         });
     }
 }
-ballsInit();
+while(!correctFlag){
+    ballsInit();
+}
 areasInit();
