650dff1d2cfc56612adbf2bdc8f3f7769e6908f5
mateusz <dalvan234@gmail.com>
2020-05-01 10:47:14 +0200

    more than one octave adding possible, but only in rising order now, buttons have different colors now

--- Piano/App.js
+++ Piano/App.js
@@ -1,19 +1,24 @@
 var audioList = document.querySelector(".audio-list");
 var buttonsList = document.querySelector(".buttons-list");
-var c3, cis3, d3, dis3, e3, f3, fis3, g3, gis3, a3, ais3, b3, c4;
-var octave3 = [c3, cis3, d3, dis3, e3, f3, fis3, g3, gis3, a3, ais3, b3, c4];
-var txtOctave3 = ["c3", "cis3", "d3", "dis3", "e3", "f3", "fis3", "g3", "gis3", "a3", "ais3", "b3", "c4"];
-
-//creating buttons
-for (var i = 0; i < octave3.length; i++) {
-  octave3[i] = document.createElement("button");
-  octave3[i].innerHTML = txtOctave3[i];
-  buttonsList.appendChild(octave3[i]);
+var options = document.querySelector(".options");
+var c3, cis3, d3, dis3, e3, f3, fis3, g3, gis3, a3, ais3, b3, c4,cis4,d4,dis4,e4,f4,fis4,g4,gis4,a4,ais4,b4,c5, cis5, d5, dis5, e5, f5, fis5, g5, gis5, a5, ais5, b5;
+var octave = [c3, cis3, d3, dis3, e3, f3, fis3, g3, gis3, a3, ais3, b3,c4, cis4, d4, dis4, e4, f4, fis4, g4, gis4, a4, ais4, b4,c5, cis5, d5, dis5, e5, f5, fis5, g5, gis5, a5, ais5, b5];
+var txtOctave = ["c3", "cis3", "d3", "dis3", "e3", "f3", "fis3", "g3", "gis3", "a3", "ais3", "b3","c4", "cis4", "d4", "dis4", "e4", "f4", "fis4", "g4", "gis4", "a4", "ais4", "b4","c5", "cis5", "d5", "dis5", "e5", "f5", "fis5", "g5", "gis5", "a5", "ais5", "b5"];
+const octaveInit = (startButtonIndex) =>{
+  //creating buttons
+for (var i = startButtonIndex-1; i < startButtonIndex+11; i++) {
+  octave[i] = document.createElement("button");
+  octave[i].innerHTML = txtOctave[i];
+  if(txtOctave[i].includes("is")){
+    octave[i].style.backgroundColor='black';
+    octave[i].style.color='white';
+  }
+  buttonsList.appendChild(octave[i]);
 }
 // copying c3-audio
-for (var i = 0; i < octave3.length-1; i++) {
+for (var i = startButtonIndex-1; i < startButtonIndex+11; i++) {
   var tmp = audioList.children[i].cloneNode(true);
-  tmp.firstElementChild.src = "C:/Muzyczka/Notes/"+txtOctave3[i+1]+".mp3";
+  tmp.firstElementChild.src = "C:/Muzyczka/Notes/"+txtOctave[i+1]+".mp3";
   audioList.appendChild(tmp);
 }
 //
@@ -23,19 +28,29 @@ const playing = (number)=>{
 }
 //adding listeners to buttons
 
-  buttonsList.children[0].addEventListener('click', () => playing(0));
-  buttonsList.children[1].addEventListener('click', () => playing(1))
-  buttonsList.children[2].addEventListener('click', () => playing(2))
-  buttonsList.children[3].addEventListener('click', () => playing(3))
-  buttonsList.children[4].addEventListener('click', () => playing(4))
-  buttonsList.children[5].addEventListener('click', () => playing(5))
-  buttonsList.children[6].addEventListener('click', () => playing(6))
-  buttonsList.children[7].addEventListener('click', () => playing(7))
-  buttonsList.children[8].addEventListener('click', () => playing(8))
-  buttonsList.children[9].addEventListener('click', () => playing(9))
-  buttonsList.children[10].addEventListener('click', () => playing(10))
-  buttonsList.children[11].addEventListener('click', () => playing(11))
-  buttonsList.children[12].addEventListener('click', () => playing(12))
-
+  buttonsList.children[startButtonIndex-1].addEventListener('click', () => playing(startButtonIndex-1));
+  buttonsList.children[startButtonIndex].addEventListener('click', () => playing(startButtonIndex))
+  buttonsList.children[startButtonIndex+1].addEventListener('click', () => playing(startButtonIndex+1))
+  buttonsList.children[startButtonIndex+2].addEventListener('click', () => playing(startButtonIndex+2))
+  buttonsList.children[startButtonIndex+3].addEventListener('click', () => playing(startButtonIndex+3))
+  buttonsList.children[startButtonIndex+4].addEventListener('click', () => playing(startButtonIndex+4))
+  buttonsList.children[startButtonIndex+5].addEventListener('click', () => playing(startButtonIndex+5))
+  buttonsList.children[startButtonIndex+6].addEventListener('click', () => playing(startButtonIndex+6))
+  buttonsList.children[startButtonIndex+7].addEventListener('click', () => playing(startButtonIndex+7))
+  buttonsList.children[startButtonIndex+8].addEventListener('click', () => playing(startButtonIndex+8))
+  buttonsList.children[startButtonIndex+9].addEventListener('click', () => playing(startButtonIndex+9))
+  buttonsList.children[startButtonIndex+10].addEventListener('click', () => playing(startButtonIndex+10))
 //
+}
+options.children[0].addEventListener('click', ()=>{
+  octaveInit(1);
+})
+options.children[1].addEventListener('click', ()=>{
+  octaveInit(13);
+})
+options.children[2].addEventListener('click', ()=>{
+  octaveInit(25);
+})
 //https://gomakethings.com/why-you-shouldnt-attach-event-listeners-in-a-for-loop-with-vanilla-javascript/
+
+//octaveInit(octave4,txtOctave4);
