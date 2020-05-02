f12425a03996d8d627923c14eea8f2b25d26c723
mateusz <dalvan234@gmail.com>
2020-05-02 23:15:48 +0200

    color of button changes for a moment after click

--- Piano/App.js
+++ Piano/App.js
@@ -12,8 +12,19 @@ let octave5 = [c5, cis5, d5, dis5, e5, f5, fis5, g5, gis5, a5, ais5, b5];
 let txtOctave3 = ["c3", "cis3", "d3", "dis3", "e3", "f3", "fis3", "g3", "gis3", "a3", "ais3", "b3"];
 let txtOctave4 = ["c4", "cis4", "d4", "dis4", "e4", "f4", "fis4", "g4", "gis4", "a4", "ais4", "b4"];
 let txtOctave5 = ["c5", "cis5", "d5", "dis5", "e5", "f5", "fis5", "g5", "gis5", "a5", "ais5", "b5"];
-const playing = (number,audioList) => {
+const playing = (number, audioList, buttonsList) => {
+  buttonsList.children[number].style.backgroundColor = "gray";
+  buttonsList.children[number].style.color = "gray";
   audioList.children[number].play();
+  setTimeout(() => {
+    if (buttonsList.children[number].id === "black-button") {
+      buttonsList.children[number].style.backgroundColor = "black";
+      buttonsList.children[number].style.color = "white";
+    } else {
+      buttonsList.children[number].style.backgroundColor = "white";
+      buttonsList.children[number].style.color = "black";
+    }
+  }, 100);
 }
 const octaveInit = (octave, txtOctave, audioList, buttonsList) => {
   //creating buttons
@@ -21,10 +32,9 @@ const octaveInit = (octave, txtOctave, audioList, buttonsList) => {
     octave[i] = document.createElement("button");
     octave[i].innerHTML = txtOctave[i];
     if (txtOctave[i].includes("is")) {
-      octave[i].id="black-button";
-    }else{
-      octave[i].id="white-button";
-      
+      octave[i].id = "black-button";
+    } else {
+      octave[i].id = "white-button";
     }
     buttonsList.appendChild(octave[i]);
   }
@@ -35,21 +45,21 @@ const octaveInit = (octave, txtOctave, audioList, buttonsList) => {
     audioList.appendChild(tmp);
   }
   //
-  
+
   //adding listeners to buttons
 
-  buttonsList.children[0].addEventListener('click', () => playing(0,audioList))
-  buttonsList.children[1].addEventListener('click', () => playing(1,audioList))
-  buttonsList.children[2].addEventListener('click', () => playing(2,audioList))
-  buttonsList.children[3].addEventListener('click', () => playing(3,audioList))
-  buttonsList.children[4].addEventListener('click', () => playing(4,audioList))
-  buttonsList.children[5].addEventListener('click', () => playing(5,audioList))
-  buttonsList.children[6].addEventListener('click', () => playing(6,audioList))
-  buttonsList.children[7].addEventListener('click', () => playing(7,audioList))
-  buttonsList.children[8].addEventListener('click', () => playing(8,audioList))
-  buttonsList.children[9].addEventListener('click', () => playing(9,audioList))
-  buttonsList.children[10].addEventListener('click', () => playing(10,audioList))
-  buttonsList.children[11].addEventListener('click', () => playing(11,audioList));
+  buttonsList.children[0].addEventListener('click', () => playing(0, audioList, buttonsList))
+  buttonsList.children[1].addEventListener('click', () => playing(1, audioList, buttonsList))
+  buttonsList.children[2].addEventListener('click', () => playing(2, audioList, buttonsList))
+  buttonsList.children[3].addEventListener('click', () => playing(3, audioList, buttonsList))
+  buttonsList.children[4].addEventListener('click', () => playing(4, audioList, buttonsList))
+  buttonsList.children[5].addEventListener('click', () => playing(5, audioList, buttonsList))
+  buttonsList.children[6].addEventListener('click', () => playing(6, audioList, buttonsList))
+  buttonsList.children[7].addEventListener('click', () => playing(7, audioList, buttonsList))
+  buttonsList.children[8].addEventListener('click', () => playing(8, audioList, buttonsList))
+  buttonsList.children[9].addEventListener('click', () => playing(9, audioList, buttonsList))
+  buttonsList.children[10].addEventListener('click', () => playing(10, audioList, buttonsList))
+  buttonsList.children[11].addEventListener('click', () => playing(11, audioList, buttonsList));
   //
 }
 options.children[0].firstElementChild.addEventListener('click', () => {
@@ -81,39 +91,39 @@ options.children[2].firstElementChild.addEventListener('click', () => {
 })
 document.addEventListener('keydown', (event) => {
   const keyName = event.key;
-  switch(keyName){
-    case "1": playing(0,audioList3); break;
-    case "2": playing(1,audioList3); break;
-    case "3": playing(2,audioList3); break;
-    case "4": playing(3,audioList3); break;
-    case "5": playing(4,audioList3); break;
-    case "6": playing(5,audioList3); break;
-    case "7": playing(6,audioList3); break;
-    case "8": playing(7,audioList3); break;
-    case "9": playing(8,audioList3); break;
-    case "0": playing(9,audioList3); break;
-    case "-": playing(10,audioList3); break;
-    case "=": playing(11,audioList3); break;
-    case "q": playing(0,audioList4); break;
-    case "w": playing(1,audioList4); break;
-    case "e": playing(2,audioList4); break;
-    case "r": playing(3,audioList4); break;
-    case "t": playing(4,audioList4); break;
-    case "y": playing(5,audioList4); break;
-    case "u": playing(6,audioList4); break;
-    case "i": playing(7,audioList4); break;
-    case "o": playing(8,audioList4); break;
-    case "p": playing(9,audioList4); break;
-    case "[": playing(10,audioList4); break;
-    case "]": playing(11,audioList4); break;
-    case "a": playing(0,audioList5); break;
-    case "s": playing(1,audioList5); break;
-    case "d": playing(2,audioList5); break;
-    case "f": playing(3,audioList5); break;
-    case "g": playing(4,audioList5); break;
-    case "h": playing(5,audioList5); break;
-    case "j": playing(6,audioList5); break;
-    case "k": playing(7,audioList5); break;
-    case "l": playing(8,audioList5); break;
+  switch (keyName) {
+    case "1": if (options.children[0].firstElementChild.checked) { playing(0, audioList3, buttonsList3) }; break;
+    case "2": if (options.children[0].firstElementChild.checked) { playing(1, audioList3, buttonsList3) }; break;
+    case "3": if (options.children[0].firstElementChild.checked) { playing(2, audioList3, buttonsList3) }; break;
+    case "4": if (options.children[0].firstElementChild.checked) { playing(3, audioList3, buttonsList3) }; break;
+    case "5": if (options.children[0].firstElementChild.checked) { playing(4, audioList3, buttonsList3) }; break;
+    case "6": if (options.children[0].firstElementChild.checked) { playing(5, audioList3, buttonsList3) }; break;
+    case "7": if (options.children[0].firstElementChild.checked) { playing(6, audioList3, buttonsList3) }; break;
+    case "8": if (options.children[0].firstElementChild.checked) { playing(7, audioList3, buttonsList3) }; break;
+    case "9": if (options.children[0].firstElementChild.checked) { playing(8, audioList3, buttonsList3) }; break;
+    case "0": if (options.children[0].firstElementChild.checked) { playing(9, audioList3, buttonsList3) }; break;
+    case "-": if (options.children[0].firstElementChild.checked) { playing(10, audioList3, buttonsList3) }; break;
+    case "=": if (options.children[0].firstElementChild.checked) { playing(11, audioList3, buttonsList3) }; break;
+    case "q": if (options.children[1].firstElementChild.checked) { playing(0, audioList4, buttonsList4) }; break;
+    case "w": if (options.children[1].firstElementChild.checked) { playing(1, audioList4, buttonsList4) }; break;
+    case "e": if (options.children[1].firstElementChild.checked) { playing(2, audioList4, buttonsList4) }; break;
+    case "r": if (options.children[1].firstElementChild.checked) { playing(3, audioList4, buttonsList4) }; break;
+    case "t": if (options.children[1].firstElementChild.checked) { playing(4, audioList4, buttonsList4) }; break;
+    case "y": if (options.children[1].firstElementChild.checked) { playing(5, audioList4, buttonsList4) }; break;
+    case "u": if (options.children[1].firstElementChild.checked) { playing(6, audioList4, buttonsList4) }; break;
+    case "i": if (options.children[1].firstElementChild.checked) { playing(7, audioList4, buttonsList4) }; break;
+    case "o": if (options.children[1].firstElementChild.checked) { playing(8, audioList4, buttonsList4) }; break;
+    case "p": if (options.children[1].firstElementChild.checked) { playing(9, audioList4, buttonsList4) }; break;
+    case "[": if (options.children[1].firstElementChild.checked) { playing(10, audioList4, buttonsList4) }; break;
+    case "]": if (options.children[1].firstElementChild.checked) { playing(11, audioList4, buttonsList4) }; break;
+    case "a": if (options.children[2].firstElementChild.checked) { playing(0, audioList5, buttonsList5) }; break;
+    case "s": if (options.children[2].firstElementChild.checked) { playing(1, audioList5, buttonsList5) }; break;
+    case "d": if (options.children[2].firstElementChild.checked) { playing(2, audioList5, buttonsList5) }; break;
+    case "f": if (options.children[2].firstElementChild.checked) { playing(3, audioList5, buttonsList5) }; break;
+    case "g": if (options.children[2].firstElementChild.checked) { playing(4, audioList5, buttonsList5) }; break;
+    case "h": if (options.children[2].firstElementChild.checked) { playing(5, audioList5, buttonsList5) }; break;
+    case "j": if (options.children[2].firstElementChild.checked) { playing(6, audioList5, buttonsList5) }; break;
+    case "k": if (options.children[2].firstElementChild.checked) { playing(7, audioList5, buttonsList5) }; break;
+    case "l": if (options.children[2].firstElementChild.checked) { playing(8, audioList5, buttonsList5) }; break;
   }
 })
