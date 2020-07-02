3de52e5a72f142f5526fe4945103bf2a89e02fd2
mateusz <dalvan234@gmail.com>
2020-07-02 10:31:39 +0200

    calendar project: changing months calendar items reinit bug fixed, displaying value of chosen item, year select added

--- Calendar/app.js
+++ Calendar/app.js
@@ -1,50 +1,78 @@
 var Calendar = /** @class */ (function () {
     function Calendar() {
         this.calendar = document.querySelector('.calendar');
-        this.select = document.querySelector('select');
+        this.items;
+        this.select = document.querySelectorAll('select');
         this.months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Nov", "Oct", "Dec"];
         this.monthsDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
         this.lastDays = 31;
-        console.log(this.select);
+        this.num = 0;
+        console.log(this.select[1]);
     }
     Calendar.prototype.monthInit = function () {
         for (var i = 0; i < 12; i++) {
             var option = document.createElement('option');
             option.value = this.months[i];
             option.innerHTML = this.months[i];
-            this.select.appendChild(option);
+            this.select[1].appendChild(option);
         }
     };
-    Calendar.prototype.lastMonthDays = function () {
-        return this.lastDays;
+    Calendar.prototype.yearInit = function () {
+        for (var i = 2020; i >= 1960; i--) {
+            var option = document.createElement('option');
+            option.value = i;
+            option.innerHTML = i;
+            this.select[0].appendChild(option);
+        }
     };
-    Calendar.prototype.calendarInit = function () {
-        console.log(this.select.value);
-        var num;
+    Calendar.prototype.selectChange = function () {
+        console.log(this.select[1].value);
         for (var i = 0; i < this.months.length; i++) {
-            if (this.months[i] === this.select.value) {
-                num = i;
-                this.lastDays = this.monthsDays[num];
+            if (this.months[i] === this.select[1].value) {
+                this.num = i;
+                this.lastDays = this.monthsDays[this.num];
+                return this.lastDays;
             }
         }
-        for (var i = 1; i <= this.monthsDays[num]; i++) {
+    };
+    Calendar.prototype.calendarInit = function () {
+        var _this = this;
+        for (var i = 1; i <= this.monthsDays[this.num]; i++) {
             var div = document.createElement('div');
-            var h1 = document.createElement('h1');
             div.className = "item";
-            h1.textContent = i.toString();
-            div.appendChild(h1);
+            div.textContent = i.toString();
             this.calendar.appendChild(div);
+        }
+        this.items = document.querySelectorAll('.item');
+        console.log(this.items);
+        var _loop_1 = function (i) {
+            this_1.items[i].addEventListener("click", function () {
+                var tmp;
+                tmp = _this.items[i].childNodes[0].nodeValue + "/" + _this.select[1].value + "/" + _this.select[0].value;
+                var text = document.querySelector('.date h1');
+                console.log(text);
+                text.innerHTML = "Chosen date is: " + tmp;
+                console.log(tmp);
+            });
+        };
+        var this_1 = this;
+        for (var i = 7; i <= this.items.length; i++) {
+            _loop_1(i);
         }
     };
     return Calendar;
 }());
 var calendarObj = new Calendar();
+calendarObj.yearInit();
 calendarObj.monthInit();
 calendarObj.calendarInit();
-var days = calendarObj.lastMonthDays();
-calendarObj.select.addEventListener("change", function () {
+calendarObj.select[1].addEventListener("change", function () {
+    var days = calendarObj.selectChange();
     for (var i = 1; i <= days; i++) {
         calendarObj.calendar.removeChild(calendarObj.calendar.lastChild);
     }
     calendarObj.calendarInit();
+});
+calendarObj.calendar.addEventListener("click", function (item) {
+    console.log(item);
 });
--- Calendar/app.ts
+++ Calendar/app.ts
@@ -1,58 +1,84 @@
 class Calendar {
     calendar: any;
     select: any;
+    items: any;
     months: string[];
     monthsDays: number[];
     lastDays: number;
+    num: number;
     constructor() {
         this.calendar = document.querySelector('.calendar');
-        this.select = document.querySelector('select');
+        this.items;
+        this.select = document.querySelectorAll('select');
         this.months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Nov","Oct","Dec"];
         this.monthsDays = [31,29,31,30,31,30,31,31,30,31,30,31]
         this.lastDays=31;
-        console.log(this.select);
+        this.num=0;
+        console.log(this.select[1]);
     }
     monthInit():any{
         for (let i = 0; i < 12; i++) {
             const option: any = document.createElement('option');
             option.value=this.months[i];
             option.innerHTML=this.months[i];
-            this.select.appendChild(option);
+            this.select[1].appendChild(option);
         }
     }
-    lastMonthDays():number{
-        return this.lastDays;
+    yearInit():any{
+        for (let i = 2020; i >= 1960; i--) {
+            const option: any = document.createElement('option');
+            option.value=i;
+            option.innerHTML=i;
+            this.select[0].appendChild(option);
+        }
     }
-    calendarInit():any{
-        console.log(this.select.value);
-        let num;
+    selectChange(): number{
+        console.log(this.select[1].value);
         for(let i=0; i<this.months.length; i++){
-            if(this.months[i]===this.select.value){
-                num=i;
-                this.lastDays=this.monthsDays[num];
+            if(this.months[i]===this.select[1].value){
+                this.num=i;
+                this.lastDays=this.monthsDays[this.num];
+                return this.lastDays;
             }
         }
-        for (let i = 1; i <= this.monthsDays[num]; i++) {
+    }
+    calendarInit():any{
+        for (let i = 1; i <= this.monthsDays[this.num]; i++) {
             const div: any = document.createElement('div');
-            const h1: any = document.createElement('h1');
             div.className = "item";
-            h1.textContent = i.toString();
-            div.appendChild(h1);
+            div.textContent = i.toString();
             this.calendar.appendChild(div);
+        }
+        this.items = document.querySelectorAll('.item');
+        console.log(this.items);
+        for(let i = 7; i<=this.items.length ;i++){
+            this.items[i].addEventListener("click", ()=>{
+                let tmp;
+                tmp=this.items[i].childNodes[0].nodeValue+"/"+this.select[1].value+"/"+this.select[0].value;
+                const text = document.querySelector('.date h1')
+                console.log(text);
+                text.innerHTML="Chosen date is: "+tmp;
+                console.log(tmp);
+                });
         }
         
     }
-    
 }
 
 
 
 let calendarObj = new Calendar();
+calendarObj.yearInit();
 calendarObj.monthInit();
 calendarObj.calendarInit();
-let days = calendarObj.lastMonthDays();
-calendarObj.select.addEventListener("change", ()=>{
+calendarObj.select[1].addEventListener("change", ()=>{
+    let days: number = calendarObj.selectChange();
     for (let i = 1; i <= days; i++) {
         calendarObj.calendar.removeChild(calendarObj.calendar.lastChild);
-    }    calendarObj.calendarInit();
+    }
+    calendarObj.calendarInit();
+});
+
+calendarObj.calendar.addEventListener("click", (item)=>{
+    console.log(item);
 });
