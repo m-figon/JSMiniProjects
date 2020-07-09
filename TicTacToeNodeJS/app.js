d410b16a41c0355e9d769dd0f85ab3ec88c50f75
mateusz <dalvan234@gmail.com>
2020-07-09 18:04:59 +0200

    check if win function added, writing logs to file

--- TicTacToeNodeJS/app.js
+++ TicTacToeNodeJS/app.js
@@ -1,9 +1,13 @@
 const gameArray = [[' ', 'A', 'B', 'C'], ['1', '-', '-', '-'], ['2', '-', '-', '-'], ['3', '-', '-', '-']];
 const readline = require('readline');
+const http = require('http');
+const fs = require('fs');
+const { isRegExp } = require('util');
 const xLength = 3;
 const yLength = 3;
-let round = 0;
+let round = 1;
 let sign = '';
+let game = true;
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -16,7 +20,7 @@ const gameInit = () => {
     }
 }
 const printSign = () => {
-    (round % 2 === 0) ? sign = 'X' : sign = "O";
+    (round % 2 === 0) ? sign = 'O' : sign = "X";
     return (`${sign} player turn. Write coordinates (eg. A1) and press enter to confirm.\n`);
 }
 const printGame = () => {
@@ -25,6 +29,31 @@ const printGame = () => {
             process.stdout.write(gameArray[i][j]);
         }
         console.log('\n');
+    }
+}
+const checkIfWin = () =>{
+    for (let i = 1; i <= yLength; i++) {
+        
+        for (let j = 1; j <= xLength; j++) {
+            if(j+2<=xLength){
+                if(gameArray[i][j]===sign && gameArray[i][j+1]===sign && gameArray[i][j+2]===sign){
+                    game=false;
+                }
+            }
+            if(j+2<=xLength && i+2<=yLength){
+                if(gameArray[i][j]===sign && gameArray[i+1][j+1]===sign && gameArray[i+2][j+2]===sign){
+                    game=false;
+                }
+                if(gameArray[i][j+2]===sign && gameArray[i+1][j+1]===sign && gameArray[i+2][j]===sign){
+                    game=false;
+                }
+            }
+            if(i+2<=yLength){
+                if(gameArray[i][j]===sign && gameArray[i+1][j]===sign && gameArray[i+2][j]===sign){
+                    game=false;
+                }
+            }
+        }
     }
 }
 const newRound = () => {
@@ -38,13 +67,41 @@ const newRound = () => {
         }
         let y = parseInt(coordinates.substr(1, 1));
         gameArray[y][x] = sign;
+        let date = new Date();
+        checkIfWin();
         printGame();
-        round++;
-        newRound()
+        if(round===1){
+            fs.appendFile('TicTacToeLog.txt', "New Game\n" , function (err) {
+                if (err) return console.log(err);
+            });    
+        }
+        if(game){
+            fs.appendFile('TicTacToeLog.txt', String(date)+"/sign:"+sign+"/noWinner/"+"round:"+round+"\n" , function (err) {
+                if (err) return console.log(err);
+            });
+            round++;
+        }else{
+            fs.appendFile('TicTacToeLog.txt', String(date)+"/"+sign+"Won/"+"round:"+round+"\n" , function (err) {
+                if (err) return console.log(err);
+            });
+            console.log(sign+" Won in "+round+" round");
+            fs.appendFile('TicTacToeLog.txt', "End of Game\n" , function (err) {
+                if (err) return console.log(err);
+            });  
+        }
+        if(game){
+            newRound();
+        }
     });
 }
 gameInit();
 newRound();
-
+/*
+let server= http.createServer((req,res)=>{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Test');
+})
+server.listen(3000,'127.0.0.01');
+*/
 
 
