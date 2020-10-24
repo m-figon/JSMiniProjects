a74d22e160c00a354cac1d2d4c780569521317f4
unknown <mateusz.figon97@gmail.com>
2020-10-25 00:11:17 +0200

    removing previous move character, checking if there is empty field when there is opponent character in specific field, checking if win function added

--- DraughtsNodeJS/func.test.js
+++ DraughtsNodeJS/func.test.js
@@ -6,16 +6,20 @@ beforeEach(() => {
   app.playersInit();
   app.printBoard();
 });
-test('Test B5 move as whites', () => {
-  expect(app.makeMove('B',5,'W')).toBeTruthy();
-});
-test('Test D5 move as whites', () => {
-  expect(app.makeMove('D',5,'W')).toBeTruthy();
-});
-test('Test B5 and C4 move as whites', () => {
-  app.makeMove('B',5,'W');
-  expect(app.makeMove('C',4,'W')).toBeTruthy();
-});
+// test('Test B5 move as whites', () => {
+//   expect(app.makeMove('B',5,'W')).toBeTruthy();
+// });
+// test('Test D5 move as whites', () => {
+//   expect(app.makeMove('D',5,'W')).toBeTruthy();
+// });
+// test('Test B5 and C4 move as whites', () => {
+//   app.makeMove('B',5,'W');
+//   expect(app.makeMove('C',4,'W')).toBeTruthy();
+// });
+// test('Test B6 move as whites', () => {
+//   expect(app.makeMove('B',6,'W')).toBeFalsy();
+// });
 test('Test B6 move as whites', () => {
-  expect(app.makeMove('B',6,'W')).toBeFalsy();
+  app.makeMove('B',5,'W');
+  expect(  app.makeMove('C',4,'W')).toBeTruthy();
 });
