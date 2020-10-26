d41aea479b94572e4371c0f969f177aa464f5d0c
unknown <mateusz.figon97@gmail.com>
2020-10-26 23:07:09 +0100

    buttons are draggable only on correct areas now (not white ones and not filled ones), draughts color changing bug fixed

--- DraughtsNodeJS/game.js
+++ DraughtsNodeJS/game.js
@@ -4,38 +4,46 @@ let blackAreas = document.querySelectorAll('.black-part');
 
 console.log(whiteAreas[0]);
 const dragstartHandler = (e) => {
-    e.dataTransfer.setData("text/plain", e.target);
-    dragObject = e.target;
-  }
+  e.dataTransfer.setData("content", e.target.className);
+  dragObject = e.target;
+}
 
-  window.addEventListener('DOMContentLoaded', () => {
-    const element = document.querySelectorAll("button");
-    for(let item of element){
-        item.addEventListener("dragstart", dragstartHandler);
-        console.log(item);
+window.addEventListener('DOMContentLoaded', () => {
+  const element = document.querySelectorAll("button");
+  for (let item of element) {
+    item.addEventListener("dragstart", dragstartHandler);
+    console.log(item);
+  }
+  for (let item of blackAreas) {
+    item.addEventListener("dragover", dragoverHandler);
+    item.addEventListener("drop", (e) => { dropHandler(e, "rgb(56, 56, 56)") });
+    item.addEventListener("dragleave", (e) => { dragleaveHandler(e, "rgb(56, 56, 56)") });
+    console.log(item);
+  }
+});
+const dragoverHandler = (e) => {
+  e.preventDefault();
+  if (e.target.children.length === 0 && e.target.className === 'black-part') {
+    e.target.style.backgroundColor = "green";
+  }
+}
+const dropHandler = (e, color) => {
+  e.preventDefault();
+  console.log(e.target.className);
+  if (e.target.children.length === 0 && e.target.className === 'black-part') {
+    if (e.dataTransfer.getData("content") === 'black-button') {
+      dragObject.style.backgroundColor = "black";
+      e.target.appendChild(dragObject);
+      e.target.style.backgroundColor = color;
+    } else if (e.dataTransfer.getData("content") === 'white-button') {
+      dragObject.style.backgroundColor = "white";
+      e.target.appendChild(dragObject);
+      e.target.style.backgroundColor = color;
     }
-    for(let item of whiteAreas){
-        item.addEventListener("dragover", dragoverHandler);
-        item.addEventListener("drop", (e) => {dropHandler(e,"rgb(243, 242, 242)")});
-        item.addEventListener("dragleave", (e) => {dragleaveHandler(e,"rgb(243, 242, 242)")});
-        console.log(item);
-    }
-    for(let item of blackAreas){
-        item.addEventListener("dragover", dragoverHandler);
-        item.addEventListener("drop", (e) => {dropHandler(e,"rgb(56, 56, 56)")});
-        item.addEventListener("dragleave", (e) => {dragleaveHandler(e,"rgb(56, 56, 56)")});
-        console.log(item);
-    }
-  });
-  const dragoverHandler = (e) => {
-    e.preventDefault();
-    e.target.style.backgroundColor="green";
-   }
-   const dropHandler = (e, color) => {
-    e.preventDefault();
-    e.target.style.backgroundColor=color;
-    e.target.appendChild(dragObject);
-   }
-   const dragleaveHandler = (e,color) => {
-    e.target.style.backgroundColor=color;
-   }
+  }
+}
+const dragleaveHandler = (e, color) => {
+  if(e.target.className==='blac-part'){
+    e.target.style.backgroundColor = color;
+  }
+}
